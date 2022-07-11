23408f5e0f69046d54ef0ebaa2b9ecc711c38b63
BenBeng <hebinghua@baidu.com>
2022-07-11 17:12:17 +0800

    Update provider version to v1.14.0

--- baiducloud/connectivity/client.go
+++ baiducloud/connectivity/client.go
@@ -49,7 +49,7 @@ type ApiVersion string
 
 var goSdkMutex = sync.RWMutex{} // The Go SDK is not thread-safe
 
-var providerVersion = "1.13.0"
+var providerVersion = "1.14.0"
 
 // Client for BaiduCloudClient
 func (c *Config) Client() (*BaiduClient, error) {
