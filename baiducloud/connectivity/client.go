e09e83bfd3579ae9849613d35b38df791b03012f
BenBeng <hebinghua@baidu.com>
2022-12-29 21:45:29 +0800

    Update CHANGELOG.md

--- baiducloud/connectivity/client.go
+++ baiducloud/connectivity/client.go
@@ -71,7 +71,7 @@ type ApiVersion string
 
 var goSdkMutex = sync.RWMutex{} // The Go SDK is not thread-safe
 
-var providerVersion = "1.13.0"
+var providerVersion = "1.19.0"
 
 // Client for BaiduCloudClient
 func (c *Config) Client() (*BaiduClient, error) {
